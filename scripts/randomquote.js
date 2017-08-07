0b39c36e0b9fc9af4b09066b1cd5d9af6555e534
EdwinWenink <edwinwenink@hotmail.com>
2017-08-08 00:31:04 +0200

    fixed weird comma

--- scripts/randomquote.js
+++ scripts/randomquote.js
@@ -3,7 +3,7 @@
 		quotes[0] = "&quotEvery person takes the limits of their own field of vision for the limits of the world.&quot - Arthur Shopenhauer";
 		quotes[1] = "&quotWithout music, life would be a mistake.&quot - Friedrich Nietzsche";
 		quotes[2] = "&quotMan is the only creature who refuses to be what he is.&quot - Albert Camus";
-		quotes[3] = "&quotLife contains but two tragedies. One is not to get your heart’s desire; the other is to get it.&quot - Socrates";
+		quotes[3] = "&quotLife contains but two tragedies. One is not to get your heart's desire; the other is to get it.&quot - Socrates";
 		quotes[4] = "&quotThere is no simple answer to such a question&quot - Jacques Derrida";
 		quotes[5] = "&quotWhat cannot be said above all must not be silenced but written.&quot - Jacques Derrida";
 		quotes[6] = "&quotWhereof one cannot speak, thereof one must be silent.&quot - Ludwig Wittgenstein";
