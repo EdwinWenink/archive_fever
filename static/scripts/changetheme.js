8135a1dfb799ecd924f62b5c7101e3cf1c8728ae
EdwinWenink <edwinwenink@hotmail.com>
2019-01-15 12:10:10 +0100

    changetheme script

---
+++ static/scripts/changetheme.js
@@ -0,0 +1,20 @@
+
+function switch_style ( css_title )
+{
+// You may use this script on your site free of charge provided
+// you do not remove this notice or the URL below. Script from
+// https://www.thesitewizard.com/javascripts/change-style-sheets.shtml
+  var i, link_tag ;
+  for (i = 0, link_tag = document.getElementsByTagName("link") ;
+    i < link_tag.length ; i++ ) {
+    if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
+      link_tag[i].title) {
+      link_tag[i].disabled = true ;
+      if (link_tag[i].title == css_title) {
+        link_tag[i].disabled = false ;
+      }
+    }
+//    set_cookie( style_cookie_name, css_title,
+//     style_cookie_duration, style_domain );
+  }
+}
